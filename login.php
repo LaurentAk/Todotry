ca509313a25ee32e02d6abfcc1195a176062f32a
LaurentAk <temitayomipo21@gmail.com>
2025-04-02 12:28:49 +0000

    Update login.php

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     // Check if user exists
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt = $conn->prepare("SELECT * FROM enquiries WHERE email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result()->fetch_assoc();
